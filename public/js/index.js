219cd0e5da2fd4c6cd738b3a6e1a64993ae49517
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-01-30 17:06:07 -0500

    add slideshow

--- public/js/index.js
+++ public/js/index.js
@@ -17,3 +17,34 @@ function hideSideNav() {
 
 sideX.addEventListener('click', hideSideNav);
 topX.addEventListener('click', hideTopNav);
+
+
+var i = 0;
+var images = [];
+var nextArrow = document.getElementById('next')
+var prevArrow = document.getElementById('prev')
+images[0] = "img/beach1.jpeg"
+images[1] = "img/beach2.jpg"
+images[2] = "img/beach3.jpeg"
+
+function changeImgForward() {
+  document.slide.src = images[i];
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+}
+
+function changeImgBackward() {
+  document.slide.src = images[i];
+  if (i > 0) {
+    i--;
+  } else {
+    i = images.length - 1;
+  }
+}
+
+document.slide.src = images[0]
+prevArrow.addEventListener('click', changeImgBackward);
+nextArrow.addEventListener('click', changeImgForward);
